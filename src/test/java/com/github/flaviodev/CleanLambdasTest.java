904bd51c281654464b538e9b9ff00786e4fdaebf
Flávio de Souza <fdsdev@gmail.com>
2018-12-27 21:28:17 -0200

    testing optional vs NPE

--- src/test/java/com/github/flaviodev/CleanLambdasTest.java
+++ src/test/java/com/github/flaviodev/CleanLambdasTest.java
@@ -1,8 +1,11 @@
 package com.github.flaviodev;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +13,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.flaviodev.model.Costumer;
+import com.github.flaviodev.model.GoldCard;
 import com.github.flaviodev.model.Order;
+import com.github.flaviodev.model.Profile;
 
 public class CleanLambdasTest {
 
@@ -18,19 +24,97 @@ public class CleanLambdasTest {
 
 	@Test
 	public void shouldntHasNotDeliveredOrders() {
+		
 		List<Order> orders = new ArrayList<>();
 		boolean hasNotDeliveredOrders = orders.stream().anyMatch(order -> order.isNotDelivered());
+
 		logger.info("shouldntHasNotDeliveredOrders -> expected: false and was: {}", hasNotDeliveredOrders);
 		assertFalse(hasNotDeliveredOrders);
 	}
 	
 	@Test
 	public void shouldHasNotDeliveredOrders() {
+
 		List<Order> orders = new ArrayList<>();
 		orders.add(new Order("1", true));
 		
 		boolean hasNotDeliveredOrders = orders.stream().anyMatch(order -> order.isNotDelivered());
 		logger.info("shouldHasNotDeliveredOrders -> expected: true and was: {}", hasNotDeliveredOrders);
 		assertTrue(hasNotDeliveredOrders);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void shouldThrowNPEOnGetGoldeCardFromProfile() {
+		
+		logger.info("shouldThrowNPEOnGetGoldeCardFromProfile -> expected: NullPointerException");
+		
+		Costumer costumer = new Costumer();
+		costumer.getProfile().getGoldCardOpt();
+		
+		fail("it was expected the throw of nullpointerexception");
+	}
+	
+	@Test
+	public void shouldDiscountZeroWhenProfileIsNull() {
+
+		Costumer costumer = new Costumer();
+		BigDecimal discount = costumer.getDiscount();
+		
+		logger.info("shouldDiscountZeroWhenProfileIsNull -> expected: 0 and was: {}", discount);
+		assertEquals(BigDecimal.ZERO, discount);
+	}
+	
+	@Test
+	public void shouldDiscountZeroWhenGoldCardIsNull() {
+
+		Profile profile = new Profile();
+		profile.setName("Costumer name");
+
+		Costumer costumer = new Costumer();
+		costumer.setProfile(profile);
+		
+		BigDecimal discount = costumer.getDiscount();
+		
+		logger.info("shouldDiscountZeroWhenGoldCardIsNull -> expected: 0 and was: {}", discount);
+		assertEquals(BigDecimal.ZERO, discount);
+	}
+
+	@Test
+	public void shouldDiscountZeroWhenDiscountIsNull() {
+
+		GoldCard goldCard = new GoldCard();
+		goldCard.setNumber("0000 0000 0000 0000");
+		
+		Profile profile = new Profile();
+		profile.setName("Costumer name");
+		profile.setGoldCard(goldCard);
+
+		Costumer costumer = new Costumer();
+		costumer.setProfile(profile);
+		
+		BigDecimal discount = costumer.getDiscount();
+		
+		logger.info("shouldDiscountZeroWhenDiscountIsNull -> expected: 0 and was: {}", discount);
+		assertEquals(BigDecimal.ZERO, discount);
+	}
+	
+	@Test
+	public void shouldDiscountTenFromGoldCard() {
+
+		GoldCard goldCard = new GoldCard();
+		goldCard.setNumber("0000 0000 0000 0000");
+		goldCard.setDiscount(BigDecimal.TEN);
+		
+		Profile profile = new Profile();
+		profile.setName("Costumer name");
+		profile.setGoldCard(goldCard);
+
+		Costumer costumer = new Costumer();
+		costumer.setProfile(profile);
+		
+		BigDecimal discount = costumer.getDiscount();
+		
+		logger.info("shouldDiscountTenFromGoldCard -> expected: 10 and was: {}", discount);
+		assertEquals(BigDecimal.TEN, discount);
 	}
 }
