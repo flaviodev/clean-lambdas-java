e82db79339ab2bcdd8c9515ed3008c71ac8372df
Flávio de Souza <fdsdev@gmail.com>
2018-12-27 21:26:15 -0200

    implementing Optional<> getter

--- src/main/java/com/github/flaviodev/model/Profile.java
+++ src/main/java/com/github/flaviodev/model/Profile.java
@@ -1,15 +1,18 @@
 package com.github.flaviodev.model;
 
-import lombok.AllArgsConstructor;
+import java.util.Optional;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
-public @Getter @Setter class Profile {
-	private String id;
-	private String name;
+public class Profile {
+	private @Getter @Setter String name;	
+	
+	private @Setter GoldCard goldCard;
+	
+	public Optional<GoldCard> getGoldCardOpt() {
+		return Optional.ofNullable(goldCard);
+	}
 }
