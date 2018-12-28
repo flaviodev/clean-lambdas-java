449b3df4e4059883e5c72dce48b5193c2c210dc9
Flávio de Souza <fdsdev@gmail.com>
2018-12-27 23:19:17 -0200

    creating null-safety for helper methods using optional

--- src/main/java/com/github/flaviodev/model/Order.java
+++ src/main/java/com/github/flaviodev/model/Order.java
@@ -1,15 +1,32 @@
 package com.github.flaviodev.model;
 
-import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
-public @Getter @Setter class Order {
-	private String id;
-	private boolean notDelivered;
+public class Order {
+	private @Getter @Setter String id;
+	private @Getter @Setter boolean notDelivered;
+
+	private @Setter List<OrderItem> items;
+
+	public Optional<List<OrderItem>> getItemsOpt() {
+		return Optional.ofNullable(items);
+	}
+
+	public BigDecimal getTotalValue() {
+		return new BigDecimal(getItemsOpt().orElse(Collections.emptyList()).stream()
+				.mapToDouble(item -> item.getItemValue().doubleValue()).sum());
+	}
+
+	public BigDecimal getTotalItemsAmount() {
+		return new BigDecimal(getItemsOpt().orElse(Collections.emptyList()).stream()
+				.mapToDouble(item -> item.getAmountOpt().orElse(BigDecimal.ZERO).doubleValue()).sum());
+	}
 }
